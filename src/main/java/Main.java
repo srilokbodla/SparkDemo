2cf3ecfff341fe473f075905f8e34c6a8e4b218b
srilok <srilokbodla@gmail.com>
2017-06-05 22:04:43 +0530

    Init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        port(4567);     //setting default listner port on which our server runs
+        //sample get request routing with variable count
+        get("/getRandam/:count", (req, res) -> {
+            int count = Integer.parseInt(req.params(":count"));
+            return json(random(count));
+        });
+        /*req is a request object contains default html request object res is object that we manipulate and send to one who send request
+        req.params is used to accept parameters this is response to request , here we are sending serialized array . Here Json function is used to serialize output
+        into json string*/
+    }
+
+    public static int[] random(int count) {
+        int[] randomIntegers = new int[count];
+        for (int i = 0; i < count; i++) {
+            randomIntegers[i] = ((int) (Math.random() * 1000));
+            System.out.println(randomIntegers[i]);
+        }
+        return randomIntegers;
+    }
+
+    public static String json(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();     // objectMapper class contains methods for converting
+        return objectMapper.writeValueAsString(obj);// this covert content of object to json string
+    }
+}
